83125beea0fbb2acab4947344584cb936627b358
Rick Valdes <101678989+Rick-Vlz@users.noreply.github.com>
2024-06-28 19:04:19 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+
+echo "Hello World! UDL Team Plata Rick y Sebas";
+echo '<img src="https://th.bing.com/th/id/OIP.X9RsSw4VmEckMEIxVwjRrwAAAA?rs=1&pid=ImgDetMain" alt="Descripción de la imagen">';
